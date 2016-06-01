ba493ece0a6016899a2bd1816402ae4916095c90
Emmanuel Louisy-Gabriel <emmanuel@skycitizen.fr>
2016-06-01 22:17:21 +0200

    expe

--- lib/elixir_coach/slack.ex
+++ lib/elixir_coach/slack.ex
@@ -4,4 +4,16 @@ defmodule ElixirCoach.Slack do
   @token Application.get_env(:elixir_coach, __MODULE__)[:slack_token]
 
   def start_link, do: start_link(@token, [])
+
+  def handle_message(message = %{ type: "message", text: "ping" }, slack, state) do
+    Slack.send_message("pong", message.channel, slack)
+    IO.inspect slack
+    {:ok, state}
+  end
+
+  def handle_message(message, slack, state) do
+    {:ok, state}
+  end
+
+
 end
